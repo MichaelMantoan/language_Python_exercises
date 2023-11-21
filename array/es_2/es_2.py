e82aa7e5bb711031cbf4b1235cbbe02c67b41961
Michael Mantoan <mantoan.michael@iisviolamarchesini.edu.it>
2023-11-21 19:20:11 +0100

    es_2 correzione con commenti spiegazione

--- array/es_2/es_2.py
+++ array/es_2/es_2.py
@@ -1,5 +1,7 @@
 numeri = []
-for i in range(5):
-    num = int(input("Inserire un numero: "))
-    numeri.append(num)
-print(numeri[5::-1])
+for i in range(5):                             #range()  serve per iterare una sequenza di numeri
+    num = int(input("Inserire un numero: "))   #attraverso la funzione input() chiedo al utente di inserire dei valori 
+    numeri.append(num)                         #che vengono convertiti in intero
+print(numeri[5::-1])                           #slicing
+#utilizzo lo slicing per stampare il mio array al contrario mettendo prima il range e poi lo step che compio
+
