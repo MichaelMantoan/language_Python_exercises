27b1e5b15e7471b56727ba7ec40714c876e45518
MichaelMantoan <98811594+MichaelMantoan@users.noreply.github.com>
2023-12-06 09:00:59 +0100

    es_4 client.py corretto

--- socket/es_4/client.py
+++ socket/es_4/client.py
@@ -3,13 +3,13 @@ import sys
 
 
 if len(sys.argv)<5:
-    print("Errore negli argomenti <stringa1> <stringa2> <ip> <porta>")
+    print("Errore negli argomenti  <ip> <porta> <stringa1> <stringa2>")
     exit()
 PORT = int(sys.argv[4])             
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((sys.argv[3], PORT))
-string1 = sys.argv[1]
-string2 = sys.argv[2]
+s.connect((sys.argv[1], PORT))
+string1 = sys.argv[3]
+string2 = sys.argv[4]
 s.send(string1.encode())
 str_reverse = s.recv(1024).decode()
 print ('Stringa 1 reverse ricevuta dal server: ', str_reverse)
